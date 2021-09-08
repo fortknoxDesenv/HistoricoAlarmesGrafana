605bd249dfe130244a7624d0218734789d012625
ewertonrichieri <contato.ewertonrichieri@gmail.com>
2021-09-08 18:03:09 -0300

    inicio

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Text, View, Button, TextInput, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const logo = require('./src/image/logo.png');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={logo} style={{width:'80%', height: 100}} />
+      <TextInput style={styles.textInput} placeholder="Usuário" />
+      <TouchableOpacity style={styles.btnEntrar} >
+        <Text style={styles.txtEntrar}>ENTRAR</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +18,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D2D6D9',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+    marginTop: -40
   },
+  textInput: {
+    borderBottomWidth: 1,
+    textAlign: "center",
+    width: '80%',
+    fontSize: 20
+  },
+  btnEntrar: {
+    width: '80%',
+    height: '8%',
+    backgroundColor: '#42708C',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  txtEntrar: {
+    color: "#999F8A",
+    fontSize: 18,
+    fontWeight: 'bold',
+  }
 });
